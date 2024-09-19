e7eadd6645b01b778ef7563c7e1f7a66b14d389a
Nifemi5 <nifemiomotosho5@gmail.com>
2024-09-19 04:40:13 -0700

    changes

---
+++ script.js
@@ -0,0 +1,132 @@
+// Javascript is a programming language
+
+// javascript is a high level programming language
+
+// Javascript is an object oriented high level programming language
+
+// Javascript is an object oriented multi-paradign high level programming language
+
+// values and variables
+//Data types
+
+//1. Numbers
+//2. strings
+//3. Boolean
+
+//4. Undefined
+//5. Null
+
+// comparison operators
+//1. Greater than
+//2. Less than
+//3. greater or Equal to
+//4. less or equal
+//5. Loose Equal
+//6. strictly equal ===
+//7. loose not equal 
+//8. strictly not equal.
+
+//logical operators
+//1. AND &&
+//2. OR ||
+//3. NOT !
+
+
+// let age = 17;
+
+// // console.log(age)
+
+// if(age >= 18){
+//     console.log(" Congrat! you're an adult")
+// }else(
+//     console.log("please call your parent")
+// )
+
+
+//strings can be looked over.They are written in quotes.
+
+// let fName = "Nifemi";
+// let lName = "Omotosho";
+
+// let fullName = "A dog chased" + " " + fName + " " + "to the" + " " + lName + "'s" + " " + "house."
+
+// //template literals
+
+// let newFullName = `A dog chased ${fName} to the ${lName}'s house`;
+
+
+// console.log(fullName);
+// console.log(newFullName);
+
+//Loop
+//For Loop is the most important. initialization, condition and increment.
+
+// for(let i = 0; i < 6; i++){
+//     console.log(fName[i]);
+// }
+
+// for(let i = 5; i >= 0; i--){
+//     console.log(fName[i])
+// }
+
+//Array
+// let num = [1, 2, 3, 4, 5];
+
+
+// let room = 2023
+
+// let num = [1, 2, "Mike", 4, room];
+
+
+//array methods
+// num.push();
+// num.pop();
+// num.shift();
+// num.unshift();
+
+// for(let i = 0; i < num.length; i++){
+//     console.log(num[i])
+// }
+
+// function declaration
+// function display(){
+//     console.log("Welcome to JavaScript Functions");
+// }
+
+// // function call
+// display();
+
+// functions with parameters
+// function calcAgeNew(newbirthYear){
+//     console.log(2024 - newbirthYear);
+// }
+
+// calcAgeNew(1996);
+// calcAgeNew(1976);
+// calcAgeNew(2004);
+// calcAgeNew(2002);
+
+
+// let fName = "Nifemi";
+// let lName = "Omotosho";
+
+
+// objects
+
+// let Nifemi = {
+//     firstName: "Omotosho",
+//     lastName: "Nifemi",
+//     age: 12,
+//     pet: "dog",
+//     friend: "Kanyin"
+// };
+
+// console.log(Nifemi);
+
+
+// DOM MANIPULATION
+
+let h1 = document.querySelector("h1");
+
+// h1.textContent = "YOU ARE GREAT"
+
